3de5714e377fa38227cd307aad8c1220e5863dda
Kofi <otukof@gmail.com>
2019-12-19 09:32:18 +0000

    Convert

--- app/src/main/java/com/example/unitconverter/subclasses/FrontConstrainstLayout.kt
+++ app/src/main/java/com/example/unitconverter/subclasses/FrontConstrainstLayout.kt
@@ -13,12 +13,14 @@ class GridConstraintLayout (context: Context, attributeSet: AttributeSet? = null
     ConstraintLayout(context,attributeSet) {
 
     var i = 0
-
     override fun addView(child: View?, params: ViewGroup.LayoutParams?) {
         Log.e("called fistr","called  $i   ${child is Guideline}")
         i+=1
         super.addView(child, params)
     }
 
+    init {
+        
+    }
 
 }
